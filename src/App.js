b48da0b0e5fee5bd166ed7e24c06a8eaa1ff8a86
helen tan <helentanmz0909@gmail.com>
2022-07-03 18:53:17 +0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className='text-xl'>Hello World</h1>
-      <button className='btn'>Click</button>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
