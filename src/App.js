4c3947292e2213cf61607302e5dda6c6727e1b41
helen tan <helentanmz0909@gmail.com>
2022-07-04 14:40:45 +0800

    Basic positioning for page content

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
 
-        <main>Content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          Content
+        </main>
 
         <Footer />
       </div>
