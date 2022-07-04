d1881a526749dd39212f5ba53b631b35f8d6d122
helen tan <helentanmz0909@gmail.com>
2022-07-04 15:40:03 +0800

    minor syntax error fix

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -5,9 +5,9 @@ import PropTypes from 'prop-types'
 function Navbar({title}) {
   return (
     <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
-      <div class='container mx-auto'>
+      <div className='container mx-auto'>
 
-        <div class='flex-none px-2 mx-2'>
+        <div className='flex-none px-2 mx-2'>
           <FaGithub className='inline pr-2 text-3xl'/>
           <Link to='/' className='text-lg font-bold align-middle'>
             {title}
