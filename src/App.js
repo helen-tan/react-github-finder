54c0836d171a67a55574758aaca08a5aefe0d9f1
helen tan <helentanmz0909@gmail.com>
2022-07-04 14:37:58 +0800

    Created Footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
         <Navbar />
 
         <main>Content</main>
+
+        <Footer />
       </div>
 
     </Router>
