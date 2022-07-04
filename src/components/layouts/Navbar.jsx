fcf61820a9da4426ecd3329542ba1c6e49ce9c99
helen tan <helentanmz0909@gmail.com>
2022-07-04 14:17:52 +0800

    Added Home & About links in Navbar component

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -4,14 +4,34 @@ import PropTypes from 'prop-types'
 
 function Navbar({title}) {
   return (
-    <nav>
-      Navbar
+    <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+      <div class='container mx-auto'>
+
+        <div class='flex-none px-2 mx-2'>
+          <FaGithub className='inline pr-2 text-3xl'/>
+          <Link to='/' className='text-lg font-bold align-middle'>
+            {title}
+          </Link>
+        </div>
+
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
+            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+              Home
+            </Link>
+            <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+              About
+            </Link>
+          </div>
+        </div>
+
+      </div>
     </nav>
   )
 }
 
 Navbar.defaultProps = {
-  title: 'Githubm Finder'
+  title: 'Github Finder'
 }
 
 Navbar.propTypes = {
