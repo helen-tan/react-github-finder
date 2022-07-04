6c44aa6058ff1b8a8105b8aad70bccbcc4d64513
helen tan <helentanmz0909@gmail.com>
2022-07-04 15:38:57 +0800

    Not Found page content

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,20 @@
+import { FaHome } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='hero'>
+      <div className='text-center hero-content'>
+        <div className='max-w-lg'>
+          <h1 className='text-8xl font-bold mb-8'>Opps!</h1>
+          <p className='text-5xl mb-8'>404 - Page not found</p>
+          <Link to='/' className='btn btn-primary'>
+            <FaHome className='mr-2'/>
+            Back To Home
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
