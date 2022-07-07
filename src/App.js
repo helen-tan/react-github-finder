067262458662fe448db1927160a9b12838f1f5b9
helen tan <helentanmz0909@gmail.com>
2022-07-07 17:30:19 +0800

    Brought GithubContext into App.js and wrapped it around the components - given them access to data in Context

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Navbar from './components/layouts/Navbar'
 import Footer from './components/layouts/Footer'
+import { GithubProvider } from './context/github/GithubContext'
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} /> {/*A catch all - if user goes to any route that doesn't exist */}
-          </Routes>
-        </main>
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/notfound' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} /> {/*A catch all - if user goes to any route that doesn't exist */}
+            </Routes>
+          </main>
 
-        <Footer />
-      </div>
+          <Footer />
+        </div>
 
-    </Router>
+      </Router>
+    </GithubProvider>
   );
 }
 
