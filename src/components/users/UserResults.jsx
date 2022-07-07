e5f5649a3067a2cb63c00ab6df6199f472bf843a
helen tan <helentanmz0909@gmail.com>
2022-07-07 16:57:58 +0800

    Created UserItem component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Spinner from '../layouts/Spinner'
+import UserItem from './UserItem'
 
 function UserResults() {
   const [users, setUsers] = useState([])
@@ -28,7 +29,7 @@ function UserResults() {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map((user) => {
-          return <h3 key={user.id}>{user.login}</h3>
+          return <UserItem key={user.id} user={user}/>
         })}
       </div>
     )
