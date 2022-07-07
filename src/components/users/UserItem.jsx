3913eaa66a6c716caf8d25f08531dff261fa33f3
helen tan <helentanmz0909@gmail.com>
2022-07-07 17:09:10 +0800

    Styled UserItem cards

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,8 +1,30 @@
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-function UserItem( { user } ) {
+// Destructured the login and avatar_url from the user props
+// So that we don't have to write {user.login}. Just write {login}
+function UserItem( { user:{ login, avatar_url } } ) {
   return (
-    <div>{user.login}</div>
+    <div className='card shadow-md compact side bg-base-100'>
+      <div className="flex-row items-center space-x-4 card-body">
+        {/* Avatar */}
+        <div>
+          <div className='avatar'>
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="Profile" />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h2 className='card-title'>{login}</h2>
+          <Link className='text-base-content text-opacity-40' to={`/users/${login}`}>
+            Visit Profile
+          </Link>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
