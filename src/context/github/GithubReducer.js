88c3f96802f760d958d20416e4921f103e66a23b
helen tan <helentanmz0909@gmail.com>
2022-07-08 23:50:14 +0800

    Added explaination in comments for use of spread operator when returning a new state object

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,12 +1,13 @@
-// Reducers related to the guthub state will be here
-// Reducers are functions we use to manipulate the state
+// Reducers related to the github state will be here
+// Reducers are pure functions we use to manipulate the state.
+// It accepts 2 parameters: the current state & an action object
 // The action is an object that has a type, which is just a string
 const githubReducer = (state, action) => {
   // check the action type
   switch(action.type) {
     case 'GET_USERS':
       return {
-        ...state,
+        ...state, // The state processed by a reducer function is immutable. Therefore the reducer function always has to return a new state object. Hence, we use the JavaScript spread operator to create a new state object from the incoming state and the part we want to change. We use all the properties from the current state object for the new state object but override specific properties (e.g. lastname) for this new object.
         users: action.payload,
         loading: false
       }
