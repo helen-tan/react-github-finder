36a66aa60663f8569795bfa9755bc1f1cdecc13c
helen tan <helentanmz0909@gmail.com>
2022-07-10 23:17:35 +0800

    update the text state to whatever user input in the search bar

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,4 +1,14 @@
+import { useState } from 'react'
+
 function UserSearch() {
+  // form inputs are component level state
+  const [text, setText] = useState('')
+
+  const handleChange = (e) => {
+    // update the text state to whatever user input in the search bar
+    setText(e.target.value)
+  }
+
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       {/* Form */}
@@ -10,6 +20,8 @@ function UserSearch() {
                 type="text"
                 className="w-full pr-40 bg-gray-200 input input-lg text-black"
                 placeholder="Search"
+                value = {text}
+                onChange={handleChange}
               />
               <button type="submit" className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">
                 Go
