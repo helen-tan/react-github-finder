bb0075278e1030b4e006c510699ae17e814759ac
helen tan <helentanmz0909@gmail.com>
2022-07-11 23:06:20 +0800

    Set alert state

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -8,8 +8,26 @@ export const AlertProvider = ({children}) => {
 
   const [state, dispatch]  = useReducer(alertReducer, initialState)
 
+  // Set an alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: "SET_ALERT",
+      payload: {
+        msg,
+        type
+       }
+    })
+
+    setTimeout(() => {
+      dispatch({
+        type: 'REMOVE_ALERT'
+      })
+    }, 3000);
+  }
+
   return <AlertContext.Provider value={{
-    alert: state
+    alert: state,
+    setAlert
   }}>
     {children}
   </AlertContext.Provider>
