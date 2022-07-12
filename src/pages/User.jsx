bbba3ec844d66bb93aabfd3aec443a04f61c9618
helen tan <helentanmz0909@gmail.com>
2022-07-12 14:52:22 +0800

    Destructured user object + 'back to search' button

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,11 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import { useEffect, useContext } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import Spinner from '../components/layouts/Spinner'
 import GithubContext from '../context/github/GithubContext'
 
 function User() {
-  const { getUser, user } = useContext(GithubContext)
+  const { getUser, user, loading } = useContext(GithubContext)
   // The useParams hook returns an object of key/value pairs of the dynamic params from the current URL that were matched by the <Route path>.
   // Get all the params from the URL
   const params = useParams()
@@ -12,8 +14,38 @@ function User() {
     getUser(params.login); // Pass the login params from the URL
   }, []);
 
+  // Destructure from the user object, these:
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable
+  } = user
+
+  if(loading) {
+    return <Spinner />
+  }
+
   return (
-    <div>{user.login}</div>
+    <>
+    <div className="w-full mx-auto lg:w-10/12">
+      <div className="mb-4">
+        <Link to='/' className='btn btn-ghost'>
+          Back To Search
+        </Link>
+      </div>
+    </div>
+    </>
   )
 }
 
